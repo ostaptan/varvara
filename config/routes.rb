ed235531bdafffda312755afdf75dc4b134d88e8
Ostap256 <otan256@gmail.com>
2013-09-24 16:26:00 +0300

    refactory

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-	  get "varvara/" => "varvara/varvara#index"
-    get "varvara/routes" => "varvara/varvara#routes"
-    get "varvara/controllers" => "varvara/varvara#controllers"
-    get "varvara/models" => "varvara/varvara#models"
+	  get "varvara/" => "varvara/varvara#index", :as => "varvara_index"
+    get "varvara/routes" => "varvara/varvara#routes", :as => "varvara_routes"
+    get "varvara/controllers" => "varvara/varvara#controllers", :as => "varvara_controllers"
+    get "varvara/models" => "varvara/varvara#models", :as => "varvara_models"
     mount Varvara::Engine => "/varvara", :as => "varvara_engine"
 end
     
